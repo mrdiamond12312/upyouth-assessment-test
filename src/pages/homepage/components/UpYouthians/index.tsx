1af1873ff19a4aaee26ab7d8c1451d1a82c4420a
Nice <hoangkimc93@gmail.com>
2024-10-28 13:38:33 +0700

    feat(upyouthian): add life section

--- src/pages/homepage/components/UpYouthians/index.tsx
+++ src/pages/homepage/components/UpYouthians/index.tsx
@@ -1,9 +1,29 @@
+import { Col, Flex, Row } from 'antd/lib';
 import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import Section from '@/components/Section';
 
+interface ILocationInfo {
+  category: string;
+  images: string[];
+}
+
+const UpYouthiansLife: React.FC<ILocationInfo> = ({ category, images }) => {
+  return (
+    <div className="mx-auto flex w-full flex-col items-center gap-4 rounded-lg border border-gray-300 p-8 lg:grow">
+      <span className="text-primary-700 w-max text-3xl font-bold">{category}</span>
+      <div className="grid w-max grid-cols-2 place-items-center gap-6 md:grid-cols-3">
+        {images.map((image) => (
+          <LazyLoadImage src={image} className="h-[48px]" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const UpYouthians: React.FC = () => {
-  const locations = [
+  const locations: ILocationInfo[] = [
     {
       category: 'We live at',
       images: [
@@ -34,14 +54,18 @@ const UpYouthians: React.FC = () => {
     {
       category: 'We work at',
       images: [
-        '/countries/study/Frame 1263.png',
-        '/countries/study/Cornell.png',
-        '/countries/study/NYU.png',
-        '/countries/study/Frame 1264.png',
-        '/countries/study/Frame 1265.png',
-        '/countries/study/FullBright.png',
-        '/countries/study/BUV.png',
-        '/countries/study/FTU.png',
+        '/countries/work/McKinsey.png',
+        '/countries/work/BCG.png',
+        '/countries/work/Frame 1266.png',
+        '/countries/work/Masan.png',
+        '/countries/work/Starbucks.png',
+        '/countries/work/Genesia Ventures.png',
+        '/countries/work/Think Zone.png',
+        '/countries/work/Ericsson.png',
+        '/countries/work/Dentons.png',
+        '/countries/work/Nielsen IQ.png',
+        '/countries/work/MOMO.png',
+        '/countries/work/GIMO.png',
       ],
     },
   ];
@@ -52,6 +76,11 @@ const UpYouthians: React.FC = () => {
         description="Our leadership, members, and alumni work in VCs and startups, just like you. We're young, closely connected, and understand young founders."
         highlight="parts of startup ecosystem"
       />
+      <Flex className="mx-auto mt-4 flex w-fit flex-col gap-4 lg:flex-row">
+        {locations.map((location) => (
+          <UpYouthiansLife {...location} />
+        ))}
+      </Flex>
     </section>
   );
 };
